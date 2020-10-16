250b66d03877566f080d498c855b753f8e31a136
tade20 <tadeomachin@gmail.com>
2020-10-16 15:05:00 -0300

    ejercicio 3
    ejercicio 1,2 y 3 terminado

---
+++ Ejerciciso JS/JS/archivo.js
@@ -0,0 +1,7 @@
+alert('Hola mundo desde js externo');
+var numero_1 = 6;
+var numero_2 = 20;
+var suma = numero_1 + numero_2;
+document.write(suma);
+alert('Hola mundo desde JavaScript!');
+alert('Hola tengo: ' + suma);
