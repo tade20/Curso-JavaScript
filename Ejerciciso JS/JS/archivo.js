90a61770a9bef0a9400c8f42a35f2ee6971d0ad9
tade20 <tadeomachin@gmail.com>
2020-10-18 20:35:29 -0300

    Ejercicio 4
    Ejercicio 1,2,3,4 terminados

--- Ejerciciso JS/JS/archivo.js
+++ Ejerciciso JS/JS/archivo.js
@@ -2,6 +2,14 @@ alert('Hola mundo desde js externo');
 var numero_1 = 6;
 var numero_2 = 20;
 var suma = numero_1 + numero_2;
+var num_par = 10;
+var espar = num_par % 2;
 document.write(suma);
 alert('Hola mundo desde JavaScript!');
 alert('Hola tengo: ' + suma);
+if ( espar == 0){
+    console.log(" Es par");
+}
+else {
+    console.log(" No es par");
+}
