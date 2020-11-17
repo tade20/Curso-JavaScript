7e7e037ef3c464963bb9ab14707aaa77acc9e897
tade20 <tadeomachin@gmail.com>
2020-11-16 23:07:59 -0300

    Proyecto1
    entrega final

--- Proyecto1/JS/Login.js
+++ Proyecto1/JS/Login.js
@@ -1,39 +1,96 @@
-function usuario(CI, Nombre, Apellido, Email, Pass, lugarnac) {
+var listaUsuario = [];
+function Usuario(CI, nombre, apellido, email, contra, lugar){
     this.CI = CI;
-    this.Nombre = Nombre;
-    this.Apellido = Apellido;
-    this.Email = Email;
-    this.Pass = Pass;
-    this.lugarnac = lugarnac;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.contra = contra;
+    this.lugar = lugar;
+
 }
-var usr1 = new usuario('34859203', 'Jorge', 'Martinez', 'jorgemar@gmail.com', 'dnqw123', 'Canelones');
-var usr2 = new usuario('38204849', 'Juan', 'Perez', 'juanpe@gmail.com', 'juandsja23', 'Durazno');
-var usr3 = new usuario('59355970', 'Rodrigo', 'Franca', 'rofra@gmail.com', 'rofaqr93', 'Artigas');
-
-
-var usrs = [usr1, usr2, usr3];
-
-function validarEmail() {
-
+var boton = document.getElementById("registrarUsu");
+boton.addEventListener('click', validacion);
+function validacion(){
+    var CI1 = document.getElementById('cedula2').value;
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
     var correo = document.getElementById('Email1').value;
+    var contra1 = document.getElementById('Password1').value;
+    var contra2 = document.getElementById('Password2').value;
+        if (CI1 == "") {
+        alert("Debe ingresar una cedula");
+    }
+            else if (CI1.length != 7 && CI1.length != 8 ) {
+                alert("La cedula debe contener 8 digitos");
+        
+    } 
+
+    if (nombre == "") {
+        alert("Debe ingresar un nombre");
+        
+    }
+
+    if (apellido == "") {
+        alert("Ingrese su apellido");
+        
+    }
+
     caracter1 = correo.indexOf("@");
     caracter2 = correo.indexOf(".");
     if (caracter1 < 1 || (caracter2 - caracter1 < 2) || correo === "") {
         alert("No es una Direccion de Correo");
     }
-    validarPass();
-}
-
-function validarPass() {
-
-    var contra1 = document.getElementById('Password1').value;
-    var contra2 = document.getElementById('Password2').value;
-    
+   
+    if (contra1 == "") {
+        alert("Debe ingresar una Contraseña");
+        
+    }
     if (contra1 != contra2) {
         alert("Las Contraseñas no coinciden");
 
     }
+    let userExsit, usrData;
     
+    guardarUsr();
+    listaUsuario.forEach(element => {
+        if(CI1 == element.CI){
+            
+            document.forms['registro'].reset();
+            alert("El usuario ya existe");
+        } 
+
+    });
+    
+    listaUsuario.push(guardarUsr());
+     console.log(listaUsuario);
+     document.forms['registro'].reset();
+    
+}
+function guardarUsr(){
+    var datoUsr = {
+        CI : document.getElementById('cedula2').value,
+        nombre : document.getElementById('nombre').value,
+        apellido : document.getElementById('apellido').value,
+        email : document.getElementById('Email1').value,
+        contra : document.getElementById('Password1').value,
+        lugar : document.getElementById('lugar').value
+};
+    return datoUsr;
+}
+var boton2 = document.getElementById('Ingresar');
+boton2.addEventListener('click', Login );
+function Login(){
+    var CI2 = document.getElementById('Cedula1').value
+    var contra3 = document.getElementById('pass1').value
+
+    listaUsuario.forEach(element =>{
+        if(CI2 == element.CI && contra3 == element.contra){
+            window.location.href="menu.html";
+        } else{
+            document.forms['Log'].reset();
+            console.log("El usuario no existe");
+        }
+    })
 }
 
 
---
+++ Proyecto1/JS/menu.js
@@ -0,0 +1,24 @@
+function Reloj (){
+    var fecha = new Date();
+    var hora = fecha.getHours();
+    var minutos = fecha.getMinutes();
+    var segundos = fecha.getSeconds();
+
+
+    if (hora < 10){
+        hora = '0' + hora;
+    }
+    if (minutos < 10){
+        minutos = '0' + minutos;
+    }
+    if (segundos < 10){
+        segundos = '0' + segundos;
+    }
+    document.getElementById("hora").innerHTML = hora + " : " + minutos + " : " + segundos;
+}
+window.onload = function(){
+    setInterval(Reloj, 1000);
+}
+
+
+
